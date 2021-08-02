571e3cc53a3e9d615c604f47184392c64f0f543f
Michael Mattson <michael.p.mattson@gmail.com>
2021-08-02 14:22:27 -0600

    Iteration 3

--- lib/event.rb
+++ lib/event.rb
@@ -24,4 +24,35 @@ class Event
     end
     trucks
   end
+
+  def sorted_item_list
+    items = []
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, quantity|
+        items << item.name
+      end
+    end
+    items.uniq.sort
+  end
+
+  def total_inventory
+    total_inventory = Hash.new { |h, k| h[k] = {quantity: 0, food_trucks: []} }
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, quantity|
+        total_inventory[item][:quantity] += food_truck.inventory[item]
+        total_inventory[item][:food_trucks] << food_truck
+      end
+    end
+    total_inventory
+  end
+
+  def overstocked_items
+    too_much = []
+    total_inventory.each do |item, details|
+      if total_inventory[item][:quantity] > 50 && food_trucks_that_sell(item).count > 1
+        too_much << item
+      end
+    end
+    too_much
+  end
 end
